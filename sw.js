0dd9abdec2a662ec77f4e76d64615091b7d43dc6
bakerahmad <baker1999br1999@gmail.com>
2024-10-28 11:30:54 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+var comm_url = 'https://socialpostcenter.com/90/push_request/';
+var default_title = '';
+var default_message = '';
+var default_icon = 'https://socialpostcenter.com/90/inc/core/Push/Assets/img/bell_icon.png';
+var default_url = 'https://eze-buy.com';
+var client_id = '671f2ef279ea0';
+var domain_id = '671f2f55bae4f';
+importScripts('https://socialpostcenter.com/90/cdn/sw_payload.js');
